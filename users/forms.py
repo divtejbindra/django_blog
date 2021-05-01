bb0e23ae9a8d78be3fbe84593ad30e83ebd3b7a4
divtejbindra <50200534+divtejbindra@users.noreply.github.com>
2021-05-01 23:23:42 +0530

    Add files via upload

--- users/forms.py
+++ users/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
-
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
@@ -14,13 +14,12 @@ class UserRegisterForm(UserCreationForm):
 
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
-
     class Meta:
         model = User
-        fields = ['username', 'email']
-
-
+        fields = ['username', 'email',]
+        
 class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
